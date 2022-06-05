ff008112086857c7eb7903c1bc0b5c0f7c31843e
Sander van Ditmarsch <72652479+SVD-NL@users.noreply.github.com>
2022-06-05 13:49:20 +0200

    add countdown timer

--- Scripts/showHospTime.user.js
+++ Scripts/showHospTime.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Show time left in hospital
-// @version      0.1
+// @version      0.2
 // @downloadURL  https://github.com/SVD-NL/svdnl-torn-userscripts/raw/main/Scripts/showHospTime.user.js
 // @updateURL    https://github.com/SVD-NL/svdnl-torn-userscripts/raw/main/Scripts/showHospTime.user.js
 // @description  Add time left in hospital to faction page
@@ -10,22 +10,39 @@
 // @grant        none
 // ==/UserScript==
 
-
-//TODO: Use the seconds parameter and make it count down
 (function() {
     const addHospTime = function() {
         $('ul.table-body').find('li.table-row').each(function() {
-            let timeLeft = '';
+            let secondsLeft = '';
             $(this).find('li[title^="<b>Hospital"]').each(function() {
-                timeLeft = $(this).attr('title').match(/\d{2}:\d{2}:\d{2}/);
+                secondsLeft = $(this).attr('title').match(/(?<=data-time=')\d*/);
             });
-            if (typeof timeLeft === 'object') {
+            if (typeof secondsLeft === 'object') {
+                const countdownDate = new Date;
+                countdownDate.setSeconds(countdownDate.getSeconds() + parseInt(secondsLeft[0]));
                 $(this).find('div.table-cell.status > span').each(function() {
-                    $(this).text(timeLeft[0]);
+                    const element = $(this)
+                    setInterval(function() { hospTimer(element, countdownDate); }, 1000);
                 });
             }
         });
     }
-    setTimeout(addHospTime, 5000)
-    //setInterval(addHospTime, 1000)
+    setTimeout(addHospTime, 3000)
+
+    const hospTimer = function(element,countdownDate) {
+        console.log(element);
+        const now = new Date();
+        const timeLeft = countdownDate.getTime() - now.getTime();
+        const hours = Math.floor(timeLeft / (1000 * 60 * 60));
+        const minutes = zeroPad(Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60)),2);
+        const seconds = zeroPad(Math.floor((timeLeft % (1000 * 60)) / 1000),2);
+        const timer = `${hours}:${minutes}:${seconds}`
+        element.text(timer);
+    }
+
+    const zeroPad = function(num, size) {
+    num = num.toString();
+    while (num.length < size) num = "0" + num;
+    return num;
+}
 })();
